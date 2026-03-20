c0bb24a66c253b7b2b7c2347f854c02832a631da
KemuelJoshua <kemueljoshuamariano@gmail.com>
2026-03-21 07:58:46 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         $superAdmin->assignRole('SuperAdmin');
 
         $user = User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
         ]);
 
         $user->assignRole('Admin');
